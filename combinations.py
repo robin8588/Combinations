3ec5c16221162182b17cc72a1f1e872b515fe587
Leo <robin_8588@aliyun.com>
2021-09-02 14:25:28 +0800

    use sheet

--- combinations.py
+++ combinations.py
@@ -1,14 +1,16 @@
 import itertools
 import pandas as pd
+from datetime import datetime
+
 
 class CombinationMaker:
-    def __init__(self,column,pick):
+    def __init__(self, column, pick, sheets):
         self.columns = column
         self.pick = pick
-        self.rows = 553784
+        self.sheets = sheets
 
-    def GenExcel(self,path):
-        lists = itertools.combinations(self.columns,self.pick)
+    def GenExcel(self, path):
+        lists = itertools.combinations(self.columns, self.pick)
         row_lists = []
         for row in lists:
             dit = {}
@@ -17,9 +19,44 @@ class CombinationMaker:
             row_lists.append(dit)
         df = pd.DataFrame(row_lists, columns=self.columns)
         df.index = df.index+1
-        dfup = df[df.index <= 100]
-        dfdown = df[df.index > 1107500]
-        
-        with pd.ExcelWriter(path) as writer:
-            dfup.to_excel(writer,sheet_name = "上")
-            dfdown.to_excel(writer,sheet_name = "下")
+        rows = len(row_lists)
+        print('total rows: ', rows)
+        writer = pd.ExcelWriter(path)
+        for i in range(self.sheets):
+            min = int(rows/self.sheets) * i
+            max = int(rows/self.sheets) * (i+1)
+            if i == self.sheets-1:
+                max = rows
+            sheet_name = str(min+1)+'-'+str(max)
+            print(sheet_name,datetime.now())
+            dfpart = df[(df.index <= max) & (df.index > min)]
+            dfpart.to_excel(writer, sheet_name=sheet_name)
+            writer.save()
+
+    def GenLast(self, path):
+        lists = itertools.combinations(self.columns, self.pick)
+        row_lists = []
+        for row in lists:
+            dit = {}
+            for i in row:
+                dit[i] = i
+            row_lists.append(dit)
+        df = pd.DataFrame(row_lists, columns=self.columns)
+        df.index = df.index+1
+        rows = len(row_lists)
+        print('total rows: ', rows)
+        writer = pd.ExcelWriter(path)
+        for i in range(self.sheets):
+            if i == 3:
+                print('start: ',i,datetime.now())
+                min = int(rows/self.sheets) * i
+                max = int(rows/self.sheets) * (i+1)
+                if i == self.sheets-1:
+                    max = rows
+                sheet_name = str(min+1)+'-'+str(max)
+                print(sheet_name)
+                dfpart = df[(df.index <= max) & (df.index > min)]
+                dfpart.to_excel(writer, sheet_name=sheet_name)
+                writer.save()
+            else:
+                print('skip: ',i)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 import combinations
 
-path = "combinations.xlsx"
-columns = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33']
-pick = 6
+path = "combinations30.xlsx"
+columns = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14',
+           '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']
+pick = 7
+sheets = 4
 
-maker = combinations.CombinationMaker(columns,pick)
+maker = combinations.CombinationMaker(columns, pick, sheets)
 print(datetime.now())
 maker.GenExcel(path)
 print(datetime.now())
