3cee78f1d4cef700bbd829bbe9e00c563646c110
Leo <robin_8588@aliyun.com>
2021-09-03 12:00:00 +0800

    improve performanc

--- combinations.py
+++ combinations.py
@@ -4,12 +4,15 @@ from datetime import datetime
 
 
 class CombinationMaker:
-    def __init__(self, column, pick, sheets):
+    def __init__(self, column, pick, sheets, path):
         self.columns = column
         self.pick = pick
         self.sheets = sheets
+        self.path = path
+        self.rows = 0
 
-    def GenExcel(self, path):
+    def GetList(self):
+        print('get table: ', datetime.now())
         lists = itertools.combinations(self.columns, self.pick)
         row_lists = []
         for row in lists:
@@ -19,44 +22,25 @@ class CombinationMaker:
             row_lists.append(dit)
         df = pd.DataFrame(row_lists, columns=self.columns)
         df.index = df.index+1
-        rows = len(row_lists)
-        print('total rows: ', rows)
-        writer = pd.ExcelWriter(path)
-        for i in range(self.sheets):
-            min = int(rows/self.sheets) * i
-            max = int(rows/self.sheets) * (i+1)
-            if i == self.sheets-1:
-                max = rows
-            sheet_name = str(min+1)+'-'+str(max)
-            print(sheet_name,datetime.now())
-            dfpart = df[(df.index <= max) & (df.index > min)]
-            dfpart.to_excel(writer, sheet_name=sheet_name)
-            writer.save()
+        self.rows = len(row_lists)
+        print('set table: ', datetime.now())
+        return df
 
-    def GenLast(self, path):
-        lists = itertools.combinations(self.columns, self.pick)
-        row_lists = []
-        for row in lists:
-            dit = {}
-            for i in row:
-                dit[i] = i
-            row_lists.append(dit)
-        df = pd.DataFrame(row_lists, columns=self.columns)
-        df.index = df.index+1
-        rows = len(row_lists)
-        print('total rows: ', rows)
-        writer = pd.ExcelWriter(path)
+    def GenSheet(self, df, i):
+        file = self.path+'file'+str(i)+'.xlsx'
+        print('create: ',file)
+        writer = pd.ExcelWriter(file)
+        min = int(self.rows/self.sheets) * i
+        max = int(self.rows/self.sheets) * (i+1)
+        if i == self.sheets-1:
+            max = self.rows
+        sheet_name = str(min+1)+'-'+str(max)
+        print(sheet_name, datetime.now())
+        dfpart = df[(df.index <= max) & (df.index > min)]
+        dfpart.to_excel(writer, sheet_name=sheet_name)
+        writer.save()
+
+    def GenExcel(self, df):
+        print('total rows: ', self.rows)
         for i in range(self.sheets):
-            if i == 3:
-                print('start: ',i,datetime.now())
-                min = int(rows/self.sheets) * i
-                max = int(rows/self.sheets) * (i+1)
-                if i == self.sheets-1:
-                    max = rows
-                sheet_name = str(min+1)+'-'+str(max)
-                print(sheet_name)
-                dfpart = df[(df.index <= max) & (df.index > min)]
-                dfpart.to_excel(writer, sheet_name=sheet_name)
-                writer.save()
-            else:
-                print('skip: ',i)
+            self.GenSheet(df, i)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 import combinations
 
-path = "combinations30.xlsx"
+path = "./results/"
 columns = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14',
            '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']
 pick = 7
-sheets = 4
+sheets = 100
 
-maker = combinations.CombinationMaker(columns, pick, sheets)
-print(datetime.now())
-maker.GenExcel(path)
-print(datetime.now())
+maker = combinations.CombinationMaker(columns, pick, sheets, path)
+
+df = maker.GetList()
+
+maker.GenExcel(df)
